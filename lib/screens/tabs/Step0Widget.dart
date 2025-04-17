655ab4bc3758d9e729674a43b6ac4d55a9c4c508
subhashish-gravitones <subhashish.gravitones@gmail.com>
2025-04-17 10:41:22 +0530

    fix: googlefont import error

--- lib/screens/tabs/Step0Widget.dart
+++ lib/screens/tabs/Step0Widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import '../../components/choice_option_selector.dart';
 import '../../components/image_option_selector.dart';
 import '../../components/labeled_text_field.dart';
