938743cadd19591907710203511ef39a8c8158a8
subhashish-gravitones <subhashish.gravitones@gmail.com>
2025-04-24 20:05:21 +0530

    fix: wrong endpoint

--- lib/configs/endpoint.dart
+++ lib/configs/endpoint.dart
@@ -1,5 +1,5 @@
-// const host = 'https://api.guftagu.net/api';
-const host = 'http://10.0.2.2:8000/api';
+const host = 'https://api.guftagu.net/api';
+// const host = 'http://10.0.2.2:8000/api';
 
 enum RemoteEndpoint {
   // Authentication
