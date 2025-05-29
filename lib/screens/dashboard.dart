8ee12555bfe035f430eb7916f28fa340a39fa05a
subhashish-gravitones <subhashish.gravitones@gmail.com>
2025-05-29 20:45:34 +0530

    refactor: remove unused import for flutter/services.dart

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:guftagu_mobile/gen/assets.gen.dart';
 import 'package:guftagu_mobile/providers/tab.dart';
